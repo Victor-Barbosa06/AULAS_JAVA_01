47ee9ff4ca7d4624a1c49d52fd9b6450eadd66ef
Victor-Barbosa06 <victorfb2001@gmail.com>
2022-05-05 21:18:28 -0300

    Update Main.java

--- AULA_5/src/aula_5/Main.java
+++ AULA_5/src/aula_5/Main.java
@@ -17,9 +17,11 @@ public class Main {
 		String [][] turma = new String[8][5];
 		double [][] notas = new double[8][3];
 		
+		//entrada de dados (Nomes)
 		for(i=0;i<8;i++) {
 			turma[i][0] = JOptionPane.showInputDialog("Informe o nome do aluno "+(i+1)+":\n");
 		}
+		//entrada de dados (Notas)
 		for(i=0;i<8;i++) {
 			for(c=0;c<3;c++) {
 				notas[i][c] = Double.parseDouble(JOptionPane.showInputDialog(turma[i][0]+": Nota "+(c+1)+"\n"));
@@ -27,6 +29,7 @@ public class Main {
 				turma[i][4] = String.valueOf(Media(notas[i][0], notas[i][1], notas[i][2]));
 			}
 		}
+		//saida de dados
 		for(i=0;i<8;i++) {
 			for(c=0;i<5;i++) {
 				JOptionPane.showMessageDialog(null, turma[i][0]+":\n\nNota 1: "+turma[i][1]+"\nNota 2: "+turma[i][2]+"\nNota 3: "+turma[i][3]+"\n\nMedia: "+turma[i][4]);
